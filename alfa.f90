03ec99416a7287bbbb661a66a7044b14f56d8ab6
rwesson <rwesson@eso.org>
2014-07-22 10:04:04 -0400

    updates

--- alfa.f90
+++ alfa.f90
@@ -4,22 +4,23 @@ implicit none
 integer :: I, lineid, popnumber, gencount, IO, spectrumlength, nlines, counter
 integer :: loc1, loc2, loc
 integer :: wlength
-double precision :: temp1, temp2, gaussian, random
+real :: temp1, temp2, gaussian, gaussianflux, random, linewidth, redshift
 character*512 :: filename
+character*10 :: gettime
 
 integer :: popsize, generations
 
-double precision :: pressure ! the fraction of candidate spectra which breed in every generation
-double precision :: mutationrate ! in each generation, this fraction of lines will be doubled, and this fraction of lines will be halved
+real :: pressure ! the fraction of candidate spectra which breed in every generation
+real :: mutationrate ! in each generation, this fraction of lines will be doubled, and this fraction of lines will be halved
 
 type spectrum
-  double precision :: wavelength
-  double precision :: flux
+  real :: wavelength
+  real :: flux
 end type
 
 type templateline
-  double precision :: wavelength
-  double precision :: peak
+  real :: wavelength
+  real :: peak
 end type
 
 type(templateline), dimension(:),allocatable :: linelist
@@ -27,22 +28,117 @@ type(templateline), dimension(:,:),allocatable :: population
 type(templateline), dimension(:,:),allocatable ::  breed
 type(spectrum), dimension(:,:), allocatable :: synthspec
 type(spectrum), dimension(:), allocatable :: realspec
-double precision, dimension(:), allocatable :: continuum !linear continuum for each population member
-double precision, dimension(:), allocatable :: breedcontinuum
+type(spectrum), dimension(1) :: bestfit
+real, dimension(:), allocatable :: continuum !linear continuum for each population member
+real, dimension(:), allocatable :: breedcontinuum
 
-double precision :: weightfactor
+real :: weightfactor
 
-double precision :: null
+real :: null
 
-double precision, dimension(:), allocatable :: rms,medianrms
+real, dimension(:), allocatable :: rms,medianrms
 
-! initialise stuff
+logical :: file_exists
+
+!read files
+
+! read in spectrum to fit
+
+call get_command_argument(1,filename)
+filename=trim(filename)
+
+if (filename=="") then
+        print *,gettime(),": error: No input spectrum specified"
+        stop
+endif
+
+inquire(file=filename, exist=file_exists) ! see if the input file is present
+
+if (.not. file_exists) then
+       print *,gettime(),": error: input spectrum ",filename," does not exist"
+       stop
+endif
+
+print *, gettime()," : reading in spectrum ",filename
+
+        I = 0
+        OPEN(199, file=filename, iostat=IO, status='old')
+                DO WHILE (IO >= 0)
+                        READ(199,*,end=112) null
+                        I = I + 1
+                END DO
+        112 spectrumlength=I
+
+!then allocate and read
+
+        allocate (synthspec(spectrumlength,popsize))
+        allocate (realspec(spectrumlength))
+
+        REWIND (199)
+        DO I=1,spectrumlength
+                READ(199,*) temp1, temp2
+                synthspec(i,:)%wavelength = temp1
+                realspec(i)%wavelength = temp1
+                realspec(i)%flux = temp2
+        END DO
+        CLOSE(199)
+
+! read in template spectrum
+
+call get_command_argument(2,filename)
+filename=trim(filename)
+if (filename=="") then
+        print *,gettime(),": error: No line catalogue specified"
+        stop
+endif
+
+inquire(file=filename, exist=file_exists) ! see if the input file is present
+
+if (.not. file_exists) then
+       print *,gettime(),": error: line catalogue ",filename," does not exist"
+       stop
+endif
+
+print *,gettime()," : fitting spectrum using line catalogue ",filename
+
+        I = 0
+        OPEN(199, file=filename, iostat=IO, status='old')
+          DO WHILE (IO >= 0)
+            READ(199,*,end=110) null
+            I = I + 1
+          END DO
+
+110     nlines=I
+
+!then allocate and read
+        allocate (linelist(nlines))
+
+        REWIND (199)
+        DO I=1,nlines
+                READ(199,*) temp1, temp2
+                linelist(i)%wavelength = temp1
+                linelist(i)%peak = temp2
+        END DO
+        CLOSE(199)
+
+! First, subtract the continuum
+! in chunks of the spectrum, calculate the mean of the lowest 5% of the fluxes.
+! Take this as the continuum
+
+! do in chunks of 1/10 of the wavelength range
+
+
+
+! initialise stuff for genetics
 
 popsize=100
-generations=100
+generations=10000
 
 pressure=0.1 !pressure * popsize needs to be an integer
 mutationrate=0.1 !mutation rate * 3 needs to be less than one
+
+linewidth=0.7
+redshift=0.0
 
 allocate(continuum(popsize))
 allocate(rms(popsize))
@@ -53,9 +149,24 @@ rms=0.D0
 call init_random_seed()
 
 ! read in spectrum to fit
-! TODO replace with reading of filename from command line
 
-filename="inputspec"
+
+call get_command_argument(1,filename)
+filename=trim(filename)
+
+if (filename=="") then
+        print *,gettime(),": error: No input spectrum specified"
+        stop
+endif
+
+inquire(file=filename, exist=file_exists) ! see if the input file is present
+
+if (.not. file_exists) then
+       print *,gettime(),": error: input spectrum ",filename," does not exist"
+       stop
+endif
+
+print *, gettime()," : reading in spectrum ",filename
 
         I = 0
         OPEN(199, file=filename, iostat=IO, status='old')
@@ -80,18 +191,28 @@ filename="inputspec"
         CLOSE(199)
 
 ! read in template spectrum
-! the if conditions is to read in only the template lines that are within the wavelength range of the
-! spectrum being fitted
-! TODO replace with reading of filename from command line
-filename=trim("peaks.txt")
+
+call get_command_argument(2,filename)
+filename=trim(filename)
+if (filename=="") then
+        print *,gettime(),": error: No line catalogue specified"
+        stop
+endif
+
+inquire(file=filename, exist=file_exists) ! see if the input file is present
+
+if (.not. file_exists) then
+       print *,gettime(),": error: line catalogue ",filename," does not exist"
+       stop
+endif
+
+print *,gettime()," : fitting spectrum using line catalogue ",filename
 
         I = 0
         OPEN(199, file=filename, iostat=IO, status='old')
           DO WHILE (IO >= 0)
             READ(199,*,end=110) null
-            if (null .ge. realspec(1)%wavelength .and. null .le. realspec(spectrumlength)%wavelength) then
-              I = I + 1
-            endif
+            I = I + 1
           END DO
 
 110     nlines=I
@@ -121,6 +242,12 @@ weightfactor=2.0*maxval(realspec%flux)
 
 ! now create population of synthetic spectra
 
+do lineid=1,nlines
+  population(lineid,:)%wavelength = linelist(lineid)%wavelength
+  population(lineid,:)%peak = 0.01!0.1+random*0.5 !start small and definitely not really close to zero
+end do
+continuum(:)=0.05 !random small value for continuum
+
 do gencount=1,generations
 
 !reset stuff to zero before doing calculations
@@ -128,67 +255,44 @@ do gencount=1,generations
 rms = 0.D0
 synthspec%flux=0.D0
 
-        do popnumber=1,popsize
+do popnumber=1,popsize
 
-        !randomize fluxes for first generation
+!calculate synthetic spectra - reset to 0 before synthesizing
+!line fluxes are calculated within 7 sigma of mean
 
-            if (gencount .eq. 1) then
-                do lineid=1,nlines
-                  call RANDOM_NUMBER(random)
-                  population(lineid,popnumber)%wavelength = linelist(lineid)%wavelength
-!                  population(lineid,popnumber)%peak = (0.9+(0.2*random))*linelist(lineid)%peak !randomize to within +-10% of known flux
-                  population(lineid,popnumber)%peak = 100.!0.1+random*0.5 !start small and definitely not really close to zero
-                end do
-                call RANDOM_NUMBER(random)
-                continuum(popnumber)=random*0.05 !random small value for continuum
-            endif
-
-        !calculate synthetic spectra - reset to 0 before synthesizing
-
-            do wlength=1,spectrumlength 
-              do lineid=1,nlines 
-                synthspec(wlength,popnumber)%flux = synthspec(wlength,popnumber)%flux + &
-                &gaussian(synthspec(wlength,popnumber)%wavelength,&
-                &population(lineid,popnumber)%peak,population(lineid,popnumber)%wavelength, dble(0.7)) 
-              end do
-            end do
+  do wlength=1,spectrumlength 
+    do lineid=1,nlines
+      if (abs(population(lineid,popnumber)%wavelength - synthspec(wlength,popnumber)%wavelength) .lt. (7*linewidth)) then
+        synthspec(wlength,popnumber)%flux = synthspec(wlength,popnumber)%flux + &
+        &gaussian(synthspec(wlength,popnumber)%wavelength,&
+        &population(lineid,popnumber)%peak,population(lineid,popnumber)%wavelength, linewidth) 
+      endif
+    end do
+  end do
 
         !add continuum to line fluxes
 
-            synthspec(:,popnumber)%flux = synthspec(:,popnumber)%flux + continuum(popnumber)
+  synthspec(:,popnumber)%flux = synthspec(:,popnumber)%flux + continuum(popnumber)
 
         !now calculate "RMS" for the "models".  reset to zero first
 
-            rms(popnumber)=0.D0
+  rms(popnumber)=0.D0
 
-            do wlength=1,spectrumlength
+  do wlength=1,spectrumlength
 !least absolute difference
 !rms(popnumber) = rms(popnumber) + abs(synthspec(wlength,popnumber)%flux-realspec(wlength)%flux)
 !weighted rms
 !       rms(popnumber)=rms(popnumber)+(((synthspec(wlength,popnumber)%flux-realspec(wlength)%flux)**2) &
 !&* (1-(realspec(wlength)%flux/weightfactor)))
 !sum of ratios
-             rms(popnumber) = rms(popnumber)+(synthspec(wlength,popnumber)%flux/realspec(wlength)%flux)
-
-            end do
-!            rms(popnumber)=(rms(popnumber)/spectrumlength)**0.5
-        end do
-
-        !next, cream off the well performing models - put the population member with the lowest RMS into the breed array, replace the RMS with something very high so that it doesn't get copied twice, repeat eg 500 times to get the best half of the models
-!print *,minval(rms),maxval(rms)
-
-!print *,"#",minval(rms),maxval(rms)
-
-if (mod(gencount,100).eq.0) then 
-  PRINT *
-print *,"# generation ",gencount,": "
-  do i=1,spectrumlength
-    print *,synthspec(i,minloc(rms,1))%wavelength,synthspec(i,minloc(rms,1))%flux,synthspec(i,maxloc(rms,1))%flux
+!             rms(popnumber) = rms(popnumber)+(synthspec(wlength,popnumber)%flux/realspec(wlength)%flux)
+!simple rms
+    rms(popnumber)=rms(popnumber)+(((synthspec(wlength,popnumber)%flux-realspec(wlength)%flux)**2))
   end do
-!do i=1,nlines
-!  print *,population(i,minloc(rms,1))%wavelength,population(i,minloc(rms,1))%peak
-!end do
-end if
+
+end do
+
+        !next, cream off the well performing models - put the population member with the lowest RMS into the breed array, replace the RMS with something very high so that it doesn't get copied twice, repeat until a fraction equal to the pressure factor have been selected
 
         do i=1,int(popsize*pressure) 
           breed(:,i) = population(:,minloc(rms,1))
@@ -201,6 +305,8 @@ end if
 !random approach will mean that some models have no offspring while others might
 !have many.  Alternative approach could be to breed all adjacent pairs so that
 !every model generates one offspring.
+
+      if (gencount .ne. generations) then
 
         do i=1,popsize 
           call random_number(random)
@@ -216,57 +322,81 @@ end if
          do lineid=1,nlines !mutation of line fluxes
            call random_number(random)
 !continuous mutation:
-if (random .le. 0.10) then 
-population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * (1+(10*(0.10-random)))
-elseif (random .ge. 0.90) then
-population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * (1-(10*(random-0.90)))
-endif
+!if (random .le. 0.10) then 
+!population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * (1+(10*(0.10-random)))
+!elseif (random .ge. 0.90) then
+!population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * (1-(10*(random-0.90)))
+!endif
 !population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * &
 !& 1+(10.*(0.5-random)**7.)
 !discrete mutation:
-!           if (random .le. (0.5*mutationrate)) then 
-!             population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * 0.5
-!           elseif (random .gt. (0.5*mutationrate) .and. random .le. mutationrate) then
-!             population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * 2.0
-!           elseif (random .gt. mutationrate .and. random .le. (2.0*mutationrate)) then
-!             population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * 0.95
-!           elseif (random .gt. (2.0*mutationrate).and. random .le. (3.0*mutationrate)) then
-!             population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * 1.05
-!           endif
+           if (random .le. (0.5*mutationrate)) then 
+             population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * 0.5
+           elseif (random .gt. (0.5*mutationrate) .and. random .le. mutationrate) then
+             population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * 2.0
+           elseif (random .gt. mutationrate .and. random .le. (2.0*mutationrate)) then
+             population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * 0.95
+           elseif (random .gt. (2.0*mutationrate).and. random .le. (3.0*mutationrate)) then
+             population(lineid,popnumber)%peak = population(lineid,popnumber)%peak * 1.05
+           endif
          enddo
 
-         call random_number(random) !mutation of continuum
-if (random .le. 0.10) then
-continuum(popnumber) = continuum(popnumber) * (1+(10*(0.10-random)))
-elseif (random .ge. 0.90) then
-continuum(popnumber) = continuum(popnumber) * (1-(10*(random-0.90)))
-endif
 
-!         if (random .le. (0.5*mutationrate)) then
-!           continuum(popnumber) = continuum(popnumber) * 0.5
-!         elseif (random .gt. (0.5*mutationrate) .and. random .le. mutationrate) then
-!           continuum(popnumber) = continuum(popnumber) * 2.0
-!         elseif (random .gt. mutationrate .and. random .le. (2.0*mutationrate)) then
-!           continuum(popnumber) = continuum(popnumber) * 0.95
-!         elseif (random .gt. (2.0*mutationrate).and. random .le. (3.0*mutationrate)) then
-!           continuum(popnumber) = continuum(popnumber) * 1.05
-!         endif
+         call random_number(random) !mutation of continuum
+!if (random .le. 0.10) then
+!continuum(popnumber) = continuum(popnumber) * (1+(10*(0.10-random)))
+!elseif (random .ge. 0.90) then
+!continuum(popnumber) = continuum(popnumber) * (1-(10*(random-0.90)))
+!endif
+
+         if (random .le. (0.5*mutationrate)) then
+           continuum(popnumber) = continuum(popnumber) * 0.5
+         elseif (random .gt. (0.5*mutationrate) .and. random .le. mutationrate) then
+           continuum(popnumber) = continuum(popnumber) * 2.0
+         elseif (random .gt. mutationrate .and. random .le. (2.0*mutationrate)) then
+           continuum(popnumber) = continuum(popnumber) * 0.95
+         elseif (random .gt. (2.0*mutationrate).and. random .le. (3.0*mutationrate)) then
+           continuum(popnumber) = continuum(popnumber) * 1.05
+         endif
        enddo
 
+     endif
+
+end do
+!write out line fluxes of best fitting spectrum
+
+!do i=1,nlines
+!  print *,"#",population(i,1)%wavelength,gaussianflux(population(i,minloc(rms,1))%peak,linewidth), &
+!    & gaussianflux(population(i,maxloc(rms,1))%peak,linewidth)
+!end do
+do i=1,spectrumlength
+!  print *,i
+  print *,synthspec(i,minloc(rms,1))%wavelength,synthspec(i,minloc(rms,1))%flux
 end do
 
 end program alfa
 
-double precision function gaussian(x,a,b,c)
+real function gaussian(x,a,b,c)
 !return the value of a gaussian function with parameters a, b, and c, at a value
 !of x
   implicit none
-  double precision :: x,a,b,c
+  real :: x,a,b,c
 
   gaussian = a*exp((-(x-b)**2)/(2*c**2))
   return
 
-end function 
+end function gaussian
+
+real function gaussianflux(a,c)
+!return the integral of the gaussian, equal to a*c*(pi**0.5)
+  implicit none
+  real :: a,c,pi
+
+  pi=3.14159265359
+  gaussianflux = a*c*(pi**0.5)
+  return
+
+end function gaussianflux
 
 character*10 function gettime()
 

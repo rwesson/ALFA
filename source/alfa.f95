914a0ae4bd3482fe8aa6b14f391cf925b541c34c
rwesson <rwesson@eso.org>
2014-10-06 19:09:37 -0400

    file reading to subroutine in module

--- source/alfa.f95
+++ source/alfa.f95
@@ -1,15 +1,16 @@
 program alfa
 
+use mod_readfiles
 use mod_routines
 use mod_types
 use mod_quicksort
 
 implicit none
-integer :: I, J, lineid, popnumber, gencount, IO, spectrumlength, nlines
+integer :: I, J, lineid, popnumber, gencount, spectrumlength, nlines
 integer :: loc1, loc2
 integer :: wlength
-real :: temp1, temp2, random, continuumtemp
-character*512 :: filename
+real :: random, continuumtemp
+character*512 :: spectrumfile,linelistfile
 
 integer :: popsize, generations
 
@@ -23,122 +24,32 @@ type(spectrum), dimension(:), allocatable :: realspec
 type(spectrum), dimension(20) :: spectrumchunk
 type(spectrum), dimension(:), allocatable :: continuum
 
-real :: null
-
 real, dimension(:), allocatable :: rms
 
-logical :: file_exists
-
 !temp XXXX
-open (101,file="intermediate",status="replace")
+  open (101,file="intermediate",status="replace")
 
 ! initialise stuff for genetics
 
-popsize=100
-generations=1000
+  popsize=100
+  generations=1000
 
-pressure=0.25 !pressure * popsize needs to be an integer
+  pressure=0.25 !pressure * popsize needs to be an integer
 
-allocate(rms(popsize))
+  allocate(rms(popsize))
 
-rms=0.D0
 ! random seed
 
-call init_random_seed()
+  call init_random_seed()
 
 !read files
 
 ! read in spectrum to fit
 
-  call get_command_argument(1,filename)
+  call get_command_argument(1,spectrumfile)
+  call get_command_argument(2,linelistfile)
 
-  if (trim(filename)=="") then
-    print *,gettime()," : error: No input spectrum specified"
-    stop
-  endif
-
-  inquire(file=filename, exist=file_exists) ! see if the input file is present
-
-  if (.not. file_exists) then
-    print *,gettime()," : error: input spectrum ",trim(filename)," does not exist"
-    stop
-  else
-    print *, gettime()," : reading in spectrum ",trim(filename)
-    I = 0
-    OPEN(199, file=filename, iostat=IO, status='old')
-      DO WHILE (IO >= 0)
-      READ(199,*,end=112) null
-      I = I + 1
-    END DO
-    112 spectrumlength=I
-  endif
-
-  !then allocate and read
-
-  allocate (synthspec(spectrumlength,popsize))
-  allocate (realspec(spectrumlength))
-  allocate (continuum(spectrumlength))
-
-  REWIND (199)
-  DO I=1,spectrumlength
-    READ(199,*) temp1, temp2
-    synthspec(i,:)%wavelength = temp1
-    realspec(i)%wavelength = temp1
-    realspec(i)%flux = temp2
-  END DO
-  CLOSE(199)
-
-  ! read in template spectrum
-
-  call get_command_argument(2,filename)
-
-  if (trim(filename)=="") then
-    print *,gettime()," : error: No line catalogue specified"
-    stop
-  endif
-
-  inquire(file=filename, exist=file_exists) ! see if the input file is present
-
-  if (.not. file_exists) then
-    print *,gettime()," : error: line catalogue ",trim(filename)," does not exist"
-    stop
-  else
-    print *,gettime()," : fitting spectrum using line catalogue ",trim(filename)
-    I = 0
-    OPEN(199, file=filename, iostat=IO, status='old')
-    DO WHILE (IO >= 0)
-      READ(199,*,end=110) null
-      if (null .ge. minval(realspec%wavelength) .and. null .le. maxval(realspec%wavelength)) then
-      !only read in lines that lie within the observed wavelength range
-        I = I + 1
-      endif
-    END DO
-    110     nlines=I
-  endif
-
-  if (nlines .eq. 0) then
-    print *,gettime()," : error : Line catalogue does not overlap with input spectrum"
-    stop
-  endif
-
-!then allocate and read
-
-  allocate(referencelinelist%peak(nlines))
-  allocate(referencelinelist%wavelength(nlines))
-
-  REWIND (199)
-  I=1
-  do while (i .le. nlines)
-    READ(199,*) temp1
-    if (temp1 .ge. minval(realspec%wavelength)) then
-      referencelinelist%wavelength(i) = temp1
-      i=i+1
-    endif
-    if (temp1 .ge.  maxval(realspec%wavelength)) then
-      exit
-    endif
-  END DO
-  CLOSE(199)
+  call readfiles(spectrumfile,linelistfile,synthspec,realspec,referencelinelist,popsize,spectrumlength, nlines)
 
 ! First, subtract the continuum
 ! in 20-element chunks of the spectrum, calculate the mean of the lowest 5 points.
@@ -147,20 +58,22 @@ call init_random_seed()
 ! todo: spline fit to points
 ! or at least linear interpolation
 
-continuum%wavelength = realspec%wavelength
+  allocate(continuum(spectrumlength))
 
-do i=11,spectrumlength-10,20
-  spectrumchunk = realspec(i-10:i+9)
-  do j=1,15
-    spectrumchunk(maxloc(spectrumchunk%flux))%flux = 0.0
-  enddo
-  continuumtemp = sum(spectrumchunk%flux)/5.
-  do j=-10,9
-     continuum(i+j)%flux = continuumtemp
-  enddo
-enddo
+  continuum%wavelength = realspec%wavelength
 
-realspec%flux = realspec%flux - continuum%flux
+  do i=11,spectrumlength-10,20
+    spectrumchunk = realspec(i-10:i+9)
+    do j=1,15
+      spectrumchunk(maxloc(spectrumchunk%flux))%flux = 0.0
+    enddo
+    continuumtemp = sum(spectrumchunk%flux)/5.
+    do j=-10,9
+       continuum(i+j)%flux = continuumtemp
+    enddo
+  enddo
+
+  realspec%flux = realspec%flux - continuum%flux
 
 !allocate some more arrays
 
@@ -191,8 +104,8 @@ do gencount=1,generations
 
 !reset stuff to zero before doing calculations
 
-rms = 0.D0
 synthspec%flux=0.D0
+rms=0.D0
 
 do popnumber=1,popsize
 
@@ -223,8 +136,7 @@ end do
     breed(i) = population(minloc(rms,1))
     rms(minloc(rms,1))=1.e10
   end do
-!print *,population(minloc(rms,1))%peak(1)
-!XXXX
+
 !then, "breed" pairs
 !random approach will mean that some models have no offspring while others might
 !have many.  Alternative approach could be to breed all adjacent pairs so that
@@ -251,7 +163,7 @@ endif
         population(popnumber)%peak(lineid) = population(popnumber)%peak(lineid) * mutation()
       enddo
     enddo
-
+!XXXX
   endif
 
   if (mod(gencount,generations/10) .eq.0 .or. gencount.eq.1) then
---
+++ source/readfiles.f95
@@ -0,0 +1,106 @@
+module mod_readfiles
+use mod_types
+use mod_routines
+
+contains
+
+subroutine readfiles(spectrumfile,linelistfile,synthspec,realspec,referencelinelist,popsize,spectrumlength,nlines)
+implicit none
+
+character*512 :: spectrumfile, linelistfile
+integer :: i, popsize
+real :: input1, input2
+integer :: io, nlines, spectrumlength
+logical :: file_exists
+
+type(linelist) :: referencelinelist
+type(spectrum), dimension(:), allocatable :: realspec
+type(spectrum), dimension(:,:), allocatable :: synthspec
+
+! read in spectrum to fit
+
+  if (trim(spectrumfile)=="") then
+    print *,gettime()," : error: No input spectrum specified"
+    stop
+  endif
+
+  inquire(file=spectrumfile, exist=file_exists) ! see if the input file is present
+
+  if (.not. file_exists) then
+    print *,gettime()," : error: input spectrum ",trim(spectrumfile)," does not exist"
+    stop
+  else
+    print *, gettime()," : reading in spectrum ",trim(spectrumfile)
+    I = 0
+    OPEN(199, file=spectrumfile, iostat=IO, status='old')
+      DO WHILE (IO >= 0)
+      READ(199,*,end=112) input1
+      I = I + 1
+    END DO
+    112 spectrumlength=I
+  endif
+
+  !then allocate and read
+
+  allocate (synthspec(spectrumlength,popsize))
+  allocate (realspec(spectrumlength))
+
+  REWIND (199)
+  DO I=1,spectrumlength
+    READ(199,*) input1, input2
+    synthspec(i,:)%wavelength = input1
+    realspec(i)%wavelength = input1
+    realspec(i)%flux = input2
+  END DO
+  CLOSE(199)
+
+  if (trim(linelistfile)=="") then
+    print *,gettime()," : error: No line catalogue specified"
+    stop
+  endif
+
+  inquire(file=linelistfile, exist=file_exists) ! see if the input file is present
+
+  if (.not. file_exists) then
+    print *,gettime()," : error: line catalogue ",trim(linelistfile)," does not exist"
+    stop
+  else
+    print *,gettime()," : fitting spectrum using line catalogue ",trim(linelistfile)
+    I = 0
+    OPEN(199, file=linelistfile, iostat=IO, status='old')
+    DO WHILE (IO >= 0)
+      READ(199,*,end=110) input1
+      if (input1 .ge. minval(realspec%wavelength) .and. input1 .le.  maxval(realspec%wavelength)) then
+      !only read in lines that lie within the observed wavelength range
+        I = I + 1
+      endif
+    END DO
+    110     nlines=I
+  endif
+
+  if (nlines .eq. 0) then
+    print *,gettime()," : error : Line catalogue does not overlap with input spectrum"
+    stop
+  endif
+
+!then allocate and read
+
+  allocate(referencelinelist%peak(nlines))
+  allocate(referencelinelist%wavelength(nlines))
+
+  REWIND (199)
+  I=1
+  do while (i .le. nlines)
+    READ(199,*) input1
+    if (input1 .ge. minval(realspec%wavelength)) then
+      referencelinelist%wavelength(i) = input1
+      i=i+1
+    endif
+    if (input1 .ge.  maxval(realspec%wavelength)) then
+      exit
+    endif
+  END DO
+  CLOSE(199)
+
+end subroutine readfiles
+end module mod_readfiles

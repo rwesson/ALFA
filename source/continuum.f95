dc795e36c9317457e17af72ee82b0c3cbb3d1f8b
rwesson <rwesson@eso.org>
2014-10-10 20:03:18 -0400

    continuum-fitting window expanded from 20 to 50 pixels

--- source/continuum.f95
+++ source/continuum.f95
@@ -1,13 +1,16 @@
 module mod_continuum
 use mod_types
+use mod_quicksort
+
 contains
 
 subroutine fit_continuum(realspec, spectrumlength, continuum)
 
 implicit none
 type(spectrum), dimension(:), allocatable :: realspec
-type(spectrum), dimension(20) :: spectrumchunk
 type(spectrum), dimension(:), allocatable :: continuum
+real, dimension(51) :: spectrumchunk
+
 integer :: i, j, spectrumlength
 
 ! in 20-element chunks of the spectrum, calculate the mean of the lowest 5 points.
@@ -21,17 +24,15 @@ integer :: i, j, spectrumlength
   continuum%wavelength = realspec%wavelength
   continuum%flux=0.D0
 
-  do i=11,spectrumlength-10
-    spectrumchunk = realspec(i-10:i+10)
-    do j=1,15
-      spectrumchunk(maxloc(spectrumchunk%flux))%flux = 0.0
-    enddo
-    continuum(i)%flux = sum(spectrumchunk%flux)/5
+  do i=26,spectrumlength-25
+    spectrumchunk = realspec(i-25:i+25)%flux
+    call qsort(spectrumchunk)
+    continuum(i)%flux = sum(spectrumchunk(1:10))/10
   enddo
 !fill in the ends
 
-  continuum(1:10)%flux=continuum(11)%flux
-  continuum(spectrumlength-9:spectrumlength)%flux=continuum(spectrumlength-10)%flux
+  continuum(1:25)%flux=continuum(26)%flux
+  continuum(spectrumlength-25:spectrumlength)%flux=continuum(spectrumlength-26)%flux
 
   realspec%flux = realspec%flux - continuum%flux
 

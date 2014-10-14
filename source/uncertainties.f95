a8711c5604c73556a4e0851ca87246fd52dbad6c
rwesson <rwesson@eso.org>
2014-10-14 15:05:09 -0400

    uncertainty estimation was not correct

--- source/uncertainties.f95
+++ source/uncertainties.f95
@@ -11,33 +11,27 @@ real, dimension(:) :: rms
 type(spectrum), dimension(:,:), allocatable :: synthspec
 type(spectrum), dimension(:), allocatable :: realspec
 type(linelist), dimension(:), allocatable :: population
-real, dimension(:), allocatable :: residuals, medianresiduals
+real, dimension(:), allocatable :: residuals
 real, dimension(20) :: spectrumchunk
 real :: wavelengthsampling
 integer :: i, uncertaintywavelengthindex
 
 allocate(residuals(size(realspec)))
-allocate(medianresiduals(size(realspec)))
-
-! median filter the residuals
 
 residuals=realspec(:)%flux - synthspec(:,minloc(rms,1))%flux
 
+! in a moving 20 unit window, calculated the RMS of the residuals, excluding the
+! 5 largest (this avoids the uncertainty calculation being biased by unfitted
+! lines or large residuals from the wings of lines)
+
 do i=11,size(realspec)-10
-  spectrumchunk=residuals(i-10:i+10)
+  spectrumchunk=abs(residuals(i-10:i+10))
   call qsort(spectrumchunk)
-  medianresiduals(i)=spectrumchunk(11)
+  spectrumchunk(15:20)=0.D0
+  realspec(i)%uncertainty=((sum(spectrumchunk**2))**0.5)/15.
 end do
 
-medianresiduals(1:10)=medianresiduals(11)
-medianresiduals(size(medianresiduals)-10:size(medianresiduals))=medianresiduals(size(medianresiduals)-11)
-
-! calculate rms in moving 20 unit window
-
-do i=11,size(realspec)-10
-  spectrumchunk=medianresiduals(i-10:i+10)
-  realspec(i)%uncertainty=(sum(spectrumchunk**2))**0.5
-end do
+! fill in the ends with the closest calculated values
 
 realspec(1:10)%uncertainty=realspec(11)%uncertainty
 realspec(size(realspec%uncertainty)-10:size(realspec%uncertainty))%uncertainty=realspec(size(realspec%uncertainty)-11)%uncertainty
@@ -52,6 +46,14 @@ do i=1,size(population(1)%uncertainty)
   &*population(minloc(rms,1))%peak(i)&
   &/realspec(uncertaintywavelengthindex)%uncertainty
 end do
+
+!write out uncertainties for debugging purposes:
+!temp=minloc(rms,1)
+!open (999, file="uncertainties")
+!write (999,*) """wavelength"" ""obs. flux"" ""synth. flux"" ""residuals"" ""median-filtered residual"" ""rms"""
+!do i=1,size(realspec)
+!  write (999,*) realspec(i)%wavelength, realspec(i)%flux, synthspec(i,temp)%flux, residuals(i), medianresiduals(i), realspec(i)%uncertainty
+!end do 
 
 end subroutine get_uncertainties
 

832b6a9582001c49ce66b559adb037235c2f7d7f
rwesson <rwesson@eso.org>
2014-11-03 16:55:51 -0400

    larger window for continuum fitting, use 25th value instead of 51st

--- source/continuum.f95
+++ source/continuum.f95
@@ -9,29 +9,26 @@ subroutine fit_continuum(realspec, spectrumlength, continuum)
 implicit none
 type(spectrum), dimension(:), allocatable :: realspec
 type(spectrum), dimension(:), allocatable :: continuum
-real, dimension(51) :: spectrumchunk
+real, dimension(101) :: spectrumchunk
 integer :: i, spectrumlength
 
-! calculate the median value of 51-element chunks of the spectrum
-! Take this as the continuum
-
-! todo: spline fit to points
-! or at least linear interpolation
+! take the 25st percentile  value of 101-element chunks of the spectrum
 
   allocate(continuum(spectrumlength))
 
   continuum%wavelength = realspec%wavelength
   continuum%flux=0.D0
 
-  do i=26,spectrumlength-25
-    spectrumchunk = realspec(i-25:i+25)%flux
+  do i=51,spectrumlength-50
+    spectrumchunk = realspec(i-50:i+50)%flux
     call qsort(spectrumchunk)
-    continuum(i)%flux = spectrumchunk(26)
+    continuum(i)%flux = spectrumchunk(25)
   enddo
+
 !fill in the ends
 
-  continuum(1:25)%flux=continuum(26)%flux
-  continuum(spectrumlength-25:spectrumlength)%flux=continuum(spectrumlength-26)%flux
+  continuum(1:50)%flux=continuum(51)%flux
+  continuum(spectrumlength-50:spectrumlength)%flux=continuum(spectrumlength-51)%flux
 
   realspec%flux = realspec%flux - continuum%flux
 

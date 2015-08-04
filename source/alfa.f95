22ef20d2d3fe884b8156107d36860bf62a487bad
rwesson <rwesson@eso.org>
2015-08-03 15:28:41 -1000

    implementation of command line options for initial guesses; format of output; message if program run with no options

--- source/alfa.f95
+++ source/alfa.f95
@@ -22,8 +22,13 @@ integer :: narg
 real :: redshiftguess, resolutionguess, redshifttolerance, resolutiontolerance
 real :: blendpeak
 real :: normalisation
+real :: c
 
 logical :: normalise=.false. !false means spectrum normalised to whatever H beta is detected, true means spectrum normalised to user specified value
+
+c=299792.458 !km/s
+redshiftguess=1.0000 !default values in absence of user specificed guess
+resolutionguess=15000.
 
 ! start
 
@@ -40,8 +45,15 @@ call init_random_seed()
 ! read command line
 
 narg = IARGC() !count input arguments
-if (narg .lt. 1) then
-  print *,gettime(),": Error : file to analyse not specified"
+if (narg .eq. 0) then
+  print *,"Usage: alfa [file] [options]"
+  print *,"  [file] is an ascii file with columns for wavelength and flux"
+  print *,"  [options]:"
+  print *,"  -n [value]: normalise to Hb=100 assuming that F(Hb)=value"
+  print *,"  -redg: initial guess for the redshift of the object"
+  print *,"         values between 0 and 3 are assumed to be z"
+  print *,"         any other values are assumed to be km/s"
+  print *,"  -resg: initial guess for the resolution, lambda/delta lambda"
   stop
 endif
 
@@ -50,7 +62,6 @@ ALLOCATE (options(Narg))
 if (narg .gt. 1) then
   do i=1,Narg
     call get_command_argument(i,options(i))
-    ! no command line options implemented yet
   enddo
 endif
 
@@ -59,7 +70,19 @@ do i=1,narg
     read (options(i+1),*) normalisation
     normalise=.true.
   endif
+  if ((trim(options(i))=="-redg") .and. (i+1) .le. Narg) then
+    read (options(i+1),*) redshiftguess
+  endif
+  if ((trim(options(i))=="-resg") .and. (i+1) .le. Narg) then
+    read (options(i+1),*) resolutionguess
+  endif
 enddo
+
+!if user-specified redshift is between 0 and 3, assume it's z; otherwise km/s
+
+if (redshiftguess .lt. 0.0 .or. redshiftguess .gt. 3.0) then
+  redshiftguess=1.+(redshiftguess/c)
+endif
 
 print *,gettime(),": command line: ",trim(commandline)
 
@@ -78,10 +101,8 @@ call fit_continuum(realspec,spectrumlength, continuum)
 ! now do the fitting
 ! first get guesses for the redshift and resolution
 
-redshiftguess=1.0000
-resolutionguess=12000.
 redshifttolerance=0.003 ! maximum absolute change in 1+v/c allowed from initial guess.  0.003 = 900 km/s
-resolutiontolerance=3000. ! maximum absolute change in lambda/delta lambda allowed from initial guess
+resolutiontolerance=resolutionguess/2.0 ! maximum absolute change in lambda/delta lambda allowed from initial guess
 linelistfile="linelists/strong_optical"
 print *,gettime(),": reading in line catalogue ",trim(linelistfile)
 call readlinelist(linelistfile, referencelinelist, nlines, fittedlines, realspec)
@@ -91,11 +112,12 @@ if (nlines .eq. 0) then
   stop
 endif
 
+print "(X,A,A,F8.1,A,F7.1)",gettime(),": initial guesses for redshift and resolution: ",c*(redshiftguess-1),"km/s, R=",resolutionguess
 print *,gettime(),": estimating resolution and redshift using ",nlines," lines"
 
 call fit(realspec, referencelinelist, redshiftguess, resolutionguess, fittedspectrum, fittedlines, redshifttolerance, resolutiontolerance)
 
-print *,gettime(),": estimated redshift and resolution: ",3.e5*(fittedlines(1)%redshift-1),fittedlines(1)%resolution
+print *,gettime(),": estimated redshift and resolution: ",c*(fittedlines(1)%redshift-1),fittedlines(1)%resolution
 
 ! then again in chunks with tighter tolerance
 
@@ -224,9 +246,9 @@ open(100,file=trim(spectrumfile)//"_lines.tex")
 write(100,*) "Observed wavelength & Rest wavelength & Flux & Uncertainty & Ion & Multiplet & Lower term & Upper term & g_1 & g_2 \\"
 do i=1,totallines
   if (fittedlines(i)%blended .eq. 0 .and. fittedlines(i)%uncertainty .gt. 3.0) then
-    write (100,"(F8.2,' & ',F8.2,' & ',F12.3,' & ',F12.3,A85,2(' & ',F12.3))") fittedlines(i)%wavelength*fittedlines(i)%redshift,fittedlines(i)%wavelength,gaussianflux(fittedlines(i)%peak,(fittedlines(i)%wavelength/fittedlines(i)%resolution)), gaussianflux(fittedlines(i)%peak,(fittedlines(i)%wavelength/fittedlines(i)%resolution))/fittedlines(i)%uncertainty, fittedlines(i)%linedata, (1.0-fittedlines(i)%redshift)*3.e5, fittedlines(i)%resolution
+    write (100,"(F8.2,' & ',F8.2,' & ',F12.3,' & ',F12.3,A85,2(' & ',F12.3))") fittedlines(i)%wavelength*fittedlines(i)%redshift,fittedlines(i)%wavelength,gaussianflux(fittedlines(i)%peak,(fittedlines(i)%wavelength/fittedlines(i)%resolution)), gaussianflux(fittedlines(i)%peak,(fittedlines(i)%wavelength/fittedlines(i)%resolution))/fittedlines(i)%uncertainty, fittedlines(i)%linedata, (1.0-fittedlines(i)%redshift)*c, fittedlines(i)%resolution
   elseif (fittedlines(i)%blended .ne. 0 .and. fittedlines(fittedlines(i)%blended)%uncertainty .gt. 3.0) then
-    write (100,"(F8.2,' & ',F8.2,' &            * &            *',A85,2(' & ',F12.3))") fittedlines(i)%wavelength*fittedlines(i)%redshift,fittedlines(i)%wavelength,fittedlines(i)%linedata, (1.0-fittedlines(i)%redshift)*3.e5,fittedlines(i)%resolution
+    write (100,"(F8.2,' & ',F8.2,' &            * &            *',A85,2(' & ',F12.3))") fittedlines(i)%wavelength*fittedlines(i)%redshift,fittedlines(i)%wavelength,fittedlines(i)%linedata, (1.0-fittedlines(i)%redshift)*c,fittedlines(i)%resolution
   endif
 enddo
 close(100)
